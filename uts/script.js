f3ed08d0c2027566cc29f62fee64fc0c79abddfc
Ananda Rizki N <90127337+ArZki@users.noreply.github.com>
2024-05-12 20:45:43 +0700

    Add files via upload

---
+++ uts/script.js
@@ -0,0 +1,11 @@
+/* Hamburger menu */
+
+const navbarNav = document.querySelector(".navbar-nav")
+document.querySelector("#hamburger-menu")
+onclick = () => {
+	navbarNav.classList.toggle("active")
+}
+
+
+
+
